0d73dd29d79b90495cfe6ea9e2e06cd4a0f1d8f2
Jesper Sandell <jesp143d@zbc.dk>
2020-10-28 08:55:38 +0100

    Add project files.

---
+++ ConsoleDB/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace ConsoleDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connStr = "server=localhost;user=root;database=minecraftcoords;port=3306;password=hack2020;";
+            MySqlConnection conn = new MySqlConnection(connStr);
+
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+
+                string sql = "SELECT * FROM minecraftcoords.user";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                
+
+                while (rdr.Read())
+                {
+                    for (int i = 0; i < rdr.FieldCount; i++)
+                    {
+                        Console.Write(rdr[i] + " -- ");
+                    }
+                    Console.WriteLine();
+                }
+                rdr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            conn.Close();
+            Console.WriteLine("Done.");
+            Console.ReadKey();
+
+        }
+    }
+}
